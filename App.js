fea8899e50a9422fb9d90b2538b3af81ca077b2b
AngeloC <82951135+AngeloC3@users.noreply.github.com>
2021-09-30 20:42:06 -0400

    Finalized version of CPA1

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, } from 'react-native';
+import { StyleSheet, View, } from 'react-native';
 
-import styles, {textstyles , liststyles} from './components/stylesheet.js';
+import styles from './components/stylesheet.js';
 import StackScreens from './components/StackScreens.js';
 
 export default function App() {
